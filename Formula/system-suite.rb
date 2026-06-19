45489a6ffe3a292c4a826647e4982908dadf182f
hamimLohani <hamimlohani@gmail.com>
2026-06-19 17:10:06 +0600

    Fix system-suite formula for v1.2.1

--- Formula/system-suite.rb
+++ Formula/system-suite.rb
@@ -1,10 +1,10 @@
 class SystemSuite < Formula
   desc "Terminal-based system maintenance and monitoring toolkit"
   homepage "https://github.com/hamimLohani/system_suite"
-  url "https://github.com/hamimLohani/system_suite/archive/refs/tags/v1.2.0.tar.gz"
+  url "https://github.com/hamimLohani/system_suite/archive/refs/tags/v1.2.1.tar.gz"
+  version "1.2.1"
   sha256 "a2b55f872c316da2753bd5ef20753a3b53ec0b9b69b8fef49363225126730f3e"
   license "MIT"
-  version "1.2.1"
 
   depends_on "bash"
 
@@ -13,7 +13,7 @@ class SystemSuite < Formula
   end
 
   test do
-    assert_match "System Suite v#{version}", shell_output("#{bin}/system-suite --version")
-    system "#{bin}/system-suite", "--help"
+    assert_match "System Suite v#{version}", shell_output("#{bin/"system-suite"} --version")
+    system bin/"system-suite", "--help"
   end
 end
