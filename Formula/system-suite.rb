d11ed241762db7fc19e3d942f68e222f532208f5
hamimLohani <hamimlohani@gmail.com>
2026-06-19 16:13:20 +0600

    Update system-suite to v1.2.0

--- Formula/system-suite.rb
+++ Formula/system-suite.rb
@@ -1,10 +1,10 @@
 class SystemSuite < Formula
   desc "Terminal-based system maintenance and monitoring toolkit"
   homepage "https://github.com/hamim-24/system_suite"
-  url "https://github.com/hamim-24/system_suite/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "e9509728f4a9bf3f2f4efced44b5fb67d39f3bf92870528fee95ea5bd956a2d1"
+  url "https://github.com/hamim-24/system_suite/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "758ab09000c90b551783cd79e1530e6c0f2403b8d781fb3e44f40e001ead407c"
   license "MIT"
-  version "1.0.5"
+  version "1.2.0"
 
   depends_on "bash"
 
@@ -13,6 +13,7 @@ class SystemSuite < Formula
   end
 
   test do
+    assert_match "System Suite v#{version}", shell_output("#{bin}/system-suite --version")
     system "#{bin}/system-suite", "--help"
   end
 end
