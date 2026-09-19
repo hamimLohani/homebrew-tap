1dff1d922bdc567997103109624cce50c72ad65b
hamimLohani <hamimlohani@gmail.com>
2026-09-19 13:39:08 +0600

    feat(system-suite): update to v1.3.1

--- Formula/system-suite.rb
+++ Formula/system-suite.rb
@@ -1,19 +1,23 @@
 class SystemSuite < Formula
   desc "Terminal-based system maintenance and monitoring toolkit"
   homepage "https://github.com/hamimLohani/system_suite"
-  url "https://github.com/hamimLohani/system_suite/archive/refs/tags/v1.2.1.tar.gz"
-  version "1.2.1"
-  sha256 "a2b55f872c316da2753bd5ef20753a3b53ec0b9b69b8fef49363225126730f3e"
+  url "https://github.com/hamimLohani/system_suite/archive/refs/tags/v1.3.1.tar.gz"
+  version "1.3.1"
+  sha256 "9ffacbfb0ec79c4ea52a7eb968c640fe3ef6fe7e3859e365f92c3ca485b2969e"
   license "MIT"
+  head "https://github.com/hamimlohani/system_suite.git", branch: "main"
 
   depends_on "bash"
 
   def install
     bin.install "system_suite.sh" => "system-suite"
+    man1.install Dir["man/*.1"] if Dir.exist?("man")
+    bash_completion.install "completions/system-suite.bash" => "system-suite" if File.exist?("completions/system-suite.bash")
+    zsh_completion.install "completions/system-suite.zsh" => "_system-suite" if File.exist?("completions/system-suite.zsh")
   end
 
   test do
-    assert_match "System Suite v#{version}", shell_output("#{bin/"system-suite"} --version")
+    assert_match "System Suite", shell_output("#{bin/"system-suite"} --version")
     system bin/"system-suite", "--help"
   end
 end
